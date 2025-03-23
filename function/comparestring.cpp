9d0055af4216bd6932b1b69d728aad87ff38a49a
akar1012 <ashishkar1012@gmail.com>
2025-03-23 12:24:54 -0700

    Update comparestring.cpp

--- function/comparestring.cpp
+++ function/comparestring.cpp
@@ -29,4 +29,42 @@ int flag =0;
     }
 
     return flag;
+    
 }
+/*
+// address and pointer version
+#include <iostream>
+using namespace std; 
+int comparestr ( char *str1, char *str2);
+int main()
+{
+    int size =6;
+    int flag =0;
+    char str1[size]= " name";
+    char str2[size]= " nome";
+   flag = comparestr( str1,  str2);
+  
+   
+cout<< flag ;
+    return 0;
+}
+int comparestr ( char *str1, char *str2){
+int flag =0;
+    
+    while ( *str1 || *str2){
+    if( *str1 == *str2){
+       str1++; 
+       str2++;
+    }
+    else if ( *str1> *str2){
+        flag =1;
+        break;
+    }
+    else if ( *str1 < *str2 ){
+        flag = -1;
+        break;
+    }
+    }
+
+    return flag;
+} */
